b041b66dd6fb294ceb9b208430fd3224d23da561
munday0 <78189489+munday0@users.noreply.github.com>
2021-04-07 13:51:08 -0700

    Update CurrencyBonus.cs

--- CurrencyBonus.cs
+++ CurrencyBonus.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*Stephanie Munday
+ * ID: 2343625
+ * smunday@chapman.edu
+ * CPSC 245-01
+ * Tower Defense Sprint I
+ *
+ * Currency bonus tower script - monsters killed within radius will give extra energy
+ */
+
 public class CurrencyBonus : MonoBehaviour
 {
-    public int totalCurr = 1; //temp val
-    public int bonusCurr = 10; //temp val
-    public float rotationSpeed = 35;
+    public int TotalCurr = 1; //temp val
+    public int BonusCurr = 10; //temp val
+    private float rotationSpeed = 35;
 
     // Update is called once per frame
     void Update()
@@ -14,14 +23,15 @@ public class CurrencyBonus : MonoBehaviour
         transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World); //rotates tower in place
     }
 
+    //checks to see if monster is killed within tower radius, adds extra energy if so
     public void OnTriggerStay(Collider co) //while in the radius of the tower
     {
         if (co.GetComponent<Monster>()) //if there is a monster
         {
             Health health = co.GetComponentInChildren<Health>(); //get the monster's health
-            if (!health.isAlive) //aka if the health of the monster reaches 0
+            if (!health.IsAlive) //aka if the health of the monster reaches 0
             {
-                totalCurr += bonusCurr; //add bonus currency to total currency
+                TotalCurr += BonusCurr; //add bonus currency to total currency
                 Debug.Log("Add bonus currency");
             }
         }
